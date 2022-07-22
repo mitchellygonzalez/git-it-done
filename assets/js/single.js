31fac0a299e6800f2cef65b70c8428e7e4efb8af
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-07-22 16:28:52 -0400

    if there are any errors, here we have provided a solution

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,15 +3,19 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
 
-
   if(repoName) {
+  // display repo name on the page
     repoNameEl.textContent = repoName;
-    getRepoIssues(repoName);
 
-}
+    getRepoIssues(repoName);
+  } else {
+  // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function(repo) {
@@ -33,8 +37,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      // if not successful, redirect to the homepage
+      document.location.replace("./index.html");
     }
   });
 };
